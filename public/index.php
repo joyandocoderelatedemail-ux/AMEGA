8c07df3eb5ce0054934994e61e501a2abc6e100f
joyandocoderelatedemail-ux <joyandocoderelatedemail@gmail.com>
2026-08-13 14:41:14 +0800

    Restore standard relative paths in public/index.php for local development and universal compatibility

--- public/index.php
+++ public/index.php
@@ -4,12 +4,15 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
-if (file_exists($maintenance = '/home/vovoco5/repositories/amegatravelandtour/storage/framework/maintenance.php')) {
+// Determine if the application is under maintenance...
+if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
-require '/home/vovoco5/repositories/amegatravelandtour/vendor/autoload.php';
+// Register the Auto Loader...
+require __DIR__.'/../vendor/autoload.php';
 
-$app = require_once '/home/vovoco5/repositories/amegatravelandtour/bootstrap/app.php';
+// Bootstrap Laravel and handle the request...
+$app = require_once __DIR__.'/../bootstrap/app.php';
 
 $app->handleRequest(Request::capture());
